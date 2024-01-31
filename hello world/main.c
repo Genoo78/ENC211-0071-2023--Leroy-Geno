02302e37ede11aa6ad94b2cfcaf3fec1cc0dad3f
Genoo78 <158208650+Genoo78@users.noreply.github.com>
2024-02-01 01:24:41 +0300

    Add files via upload

---
+++ hello world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Leroy.\n");
+    printf("My course is Civil Engineering.\n" );
+    printf("My favorite team is Chelsea.\n");
+    printf("My favorite meal is pork.");
+    return 0;
+
+}
+
